b9bd22707edcb7482fb00e099f63352ec3582c42
Leandro Pereira <leandro.pereiran23@gmail.com>
2026-07-13 01:54:20 -0300

    Add improvements

--- server/prisma.config.ts
+++ server/prisma.config.ts
@@ -1,5 +1,8 @@
-import 'dotenv/config';
+import { config } from 'dotenv';
+import { expand } from 'dotenv-expand';
 import { defineConfig, env } from 'prisma/config';
+
+expand(config());
 
 export default defineConfig({
   schema: './prisma/schema.prisma',
--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -1,6 +1,9 @@
-import 'dotenv/config';
+import { config } from 'dotenv';
+import { expand } from 'dotenv-expand';
 import { PrismaClient } from "../generated/prisma/client.js";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+
+expand(config());
 
 const adapter = new PrismaMariaDb(process.env.DATABASE_URL!);
 
